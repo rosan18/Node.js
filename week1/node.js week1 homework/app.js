647a9471dd70dcee8dd5dedeec73fff5f7841e1f
rosan18 <37535853+rosan18@users.noreply.github.com>
2018-08-30 17:03:37 +0300

    Update app.js

--- week1/node.js week1 homework/app.js
+++ week1/node.js week1 homework/app.js
@@ -28,35 +28,35 @@ http.createServer(function(req,res){
    }
  res.end('listening port');
 }).listen(8080)
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, {"Content-Type": "text/html"});
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {"Content-Type": "text/html"});
   
-//   switch(req.url) {
+  switch(req.url) {
 
-//     case '/state':
-//       state;
-//       res.write(`<h2>the current state ${state}</h2>`)
-//       break;
+    case '/state':
+      state;
+      res.write(`<h2>the current state ${state}</h2>`)
+      break;
   
-//     case '/add':
-//       state++;
-//       res.write(`<h2>adding state + 1. Current statte: ${state}</h2>`)
-//       break;
+    case '/add':
+      state++;
+      res.write(`<h2>adding state + 1. Current statte: ${state}</h2>`)
+      break;
     
-//     case '/subtract':
-//       state--;
-//       res.write(`<h2>subtracting state - 1. Current statte: ${state}</h2>`)
-//       break;
+    case '/subtract':
+      state--;
+      res.write(`<h2>subtracting state - 1. Current statte: ${state}</h2>`)
+      break;
   
-//     case '/reset':
-//       state = 10;
-//       res.write(`<h2>Reset state to default: ${state}</h2>`)
-//       break;
+    case '/reset':
+      state = 10;
+      res.write(`<h2>Reset state to default: ${state}</h2>`)
+      break;
   
-//     default:
-//       res.writeHead(404, {"Content-Type": "text/html"});
-//       res.write(`<h2>code 404: page not found</h2>`)
-//       break;
-//   }
-//   res.end();
-// }).listen(PORT, console.log(`listening port: ${PORT}`));
+    default:
+      res.writeHead(404, {"Content-Type": "text/html"});
+      res.write(`<h2>code 404: page not found</h2>`)
+      break;
+  }
+  res.end();
+}).listen(PORT, console.log(`listening port: ${PORT}`))
